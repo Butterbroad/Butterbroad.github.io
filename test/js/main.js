f03aaee5635a57bd0368bd6e2a2fd6d6d06a5731
Butterbroad <pyatochevskiy@gmail.com>
2020-01-31 18:15:56 +0300

    add test

---
+++ test/js/main.js
@@ -0,0 +1,53 @@
+let isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+let body = document.querySelector('body');
+if (isMobile.any()) {
+    body.classList.add('touch');
+    let arrow = document.querySelectorAll('.arrow');
+
+
+    for (let i = 0; i < arrow.length; i++) {
+        arrow[i].addEventListener('click', function () {
+            dropdown.classList.toggle('show');
+            thisArrow.classList.toggle('active');
+            arrow[i].addEventListener('click', scroll(arrow[i]));
+        });
+        let scroll = function handleButtonClick(item) {
+            item.scrollIntoView({
+                block: "start",
+                behavior: "smooth"
+            });
+        }
+        let dropdown = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+    }
+} else {
+    body.classList.add('no-touch');
+}
+
+let burger = document.querySelector('.burger__menu');
+let navigation = document.querySelector('.menu__list');
+
+burger.addEventListener('click', function () {
+    navigation.classList.toggle('show');
+});
