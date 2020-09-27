c93510ca5eadb294b7cccc08a7ac29f9e208bbe9
Butterbroad <pyatochevskiy@gmail.com>
2020-09-28 00:35:26 +0300

    ie v3

--- league/js/script.js
+++ league/js/script.js
@@ -17,7 +17,19 @@
 'use strict';
 
 const animItems = document.querySelectorAll('.anim-item');
-
+function get_name_browser() {
+    let ua = navigator.userAgent;
+    if (ua.search(/YaBrowser/) > 0) return 'Яндекс Браузер';
+    if (ua.search(/rv:11.0/) > 0) return 'Internet Explorer 11';
+    if (ua.search(/MSIE/) > 0) return 'Internet Explorer';
+    if (ua.search(/Edge/) > 0) return 'Edge';
+    if (ua.search(/Chrome/) > 0) return 'Google Chrome';
+    if (ua.search(/Firefox/) > 0) return 'Firefox';
+    if (ua.search(/Opera/) > 0) return 'Opera';
+    if (ua.search(/Safari/) > 0) return 'Safari';
+    return 'Не определен';
+}
+let browser = get_name_browser();
 
 if (animItems.length > 0) {
 
@@ -42,8 +54,8 @@ if (animItems.length > 0) {
                     animItem.classList.remove('active');
                 }
             }
-            let ua = navigator.userAgent;
-            if (ua.search(/rv:11.0/) || ua.search(/MSIE/) || ua.search(/Edge/)) {
+
+            if (browser == 'Edge' || browser == 'Internet Explorer') {
                 animItem.classList.add('active');
             }
         }
@@ -61,30 +73,10 @@ if (animItems.length > 0) {
     }, 700);
 }
 
-function get_name_browser() {
-    let ua = navigator.userAgent;
-    if (ua.search(/YaBrowser/) > 0) return 'Яндекс Браузер';
-    if (ua.search(/rv:11.0/) > 0) return 'Internet Explorer 11';
-    if (ua.search(/MSIE/) > 0) return 'Internet Explorer';
-    if (ua.search(/Edge/) > 0) return 'Edge';
-    if (ua.search(/Chrome/) > 0) return 'Google Chrome';
-    if (ua.search(/Firefox/) > 0) return 'Firefox';
-    if (ua.search(/Opera/) > 0) return 'Opera';
-    if (ua.search(/Safari/) > 0) return 'Safari';
-    return 'Не определен';
-}
 
-// let browser = get_name_browser();
-// if (browser == 'Edge' || 'Internet Explorer') {
-//     alert('Код сработал.');
-// } else {
-//     alert('Код НЕ сработал.');
-// }
 
-// let ua = navigator.userAgent;
-// if (ua.search(/rv:11.0/) || ua.search(/MSIE/) || ua.search(/Edge/)) {
-//     animItem.classList.add('active');
-// }
+
+
 
 
 function testWebP(callback) {
