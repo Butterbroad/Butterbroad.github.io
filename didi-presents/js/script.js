6d395136139ca5fffdd991963c9b437c8c46caf0
butterbroad <pyatochevskiy@gmail.com>
2021-03-19 14:25:40 +0300

    added last version

--- didi-presents/js/script.js
+++ didi-presents/js/script.js
@@ -1,62 +1,56 @@
 'use strict'
+document.addEventListener('DOMContentLoaded', () => {
 
-const sliderOne = new Swiper('.slider-one', {
-  wrapperClass: 'slider__wrapper',
-  slideClass: 'slider__item',
-  slidesPerView: 5,
-  spaceBetween: 10,
-});
+  //scroll to top btn
+  const upBtn = document.querySelector('.btn-up');
 
-const sliderTwo = new Swiper('.slider-two', {
-  wrapperClass: 'slider__wrapper',
-  slideClass: 'slider__item',
-  slidesPerView: 5,
-  spaceBetween: 10,
-});
-
-const catalogImgSlider = new Swiper('.catalog__item-slider', {
-  wrapperClass: 'catalog__item-slider-wrapper',
-  slideClass: 'catalog__item-slider-slide',
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
-
-//header nav
-const headerNavigationLinks = document.querySelectorAll('.menu__list-item');
-
-headerNavigationLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    let path = link.getAttribute("data-path");
-    slideToSection(path);
+  upBtn.addEventListener('click', () => {
+    scrollTo(0, 0);
   });
-});
 
-function slideToSection(path) {
-  let getSection = document.getElementById(path);
-  if (getSection) {
-    getSection.scrollIntoView({ block: "center", behavior: "smooth" });
+  window.addEventListener('scroll', () => {
+    let scrolled = window.pageYOffset;
+    if (scrolled > 700) {
+      upBtn.style.display = 'flex';
+    } else {
+      upBtn.style.display = 'none';
+    }
+  });
+
+  //header burger 
+  const headerBurger = document.querySelector('.header__burger');
+  headerBurger.addEventListener('click', () => {
+    headerBurger.classList.toggle('active');
+    document.body.classList.toggle('lock');
+    document.querySelector('.mobile-menu').classList.toggle('active');
+  });
+
+  //navigation
+  const headerNavigationLinks = document.querySelectorAll('.nav-link');
+  headerNavigationLinks.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      let path = link.getAttribute("data-path");
+      slideToSection(path);
+      document.body.classList.remove('lock');
+      headerBurger.classList.remove('active');
+      document.querySelector('.mobile-menu').classList.remove('active');
+    });
+  });
+
+  function slideToSection(path) {
+    let getSection = document.getElementById(path);
+    if (getSection) {
+      getSection.scrollIntoView({ block: "start", behavior: "smooth" });
+    }
+    if (getSection.classList.contains('centered')) {
+      getSection.scrollIntoView({ block: "center", behavior: "smooth" });
+    }
   }
-}
-
-//scroll to top btn
-const upBtn = document.querySelector('.btn-up');
-
-upBtn.addEventListener('click', () => {
-  scrollTo(0, 0);
-});
-
-window.addEventListener('scroll', () => {
-  let scrolled = window.pageYOffset;
-  if (scrolled > 700) {
-    upBtn.style.display = 'flex';
-  } else {
-    upBtn.style.display = 'none';
-  }
-});
 
 
-function testWebP(callback) {
+
+  function testWebP(callback) {
 
     var webP = new Image();
     webP.onload = webP.onerror = function () {
@@ -72,4 +66,6 @@ testWebP(function (support) {
     } else {
         document.querySelector('body').classList.add('no-webp');
     }
+});
+
 });
